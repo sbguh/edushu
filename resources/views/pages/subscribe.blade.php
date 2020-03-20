f7db4c8a52b7f6efe87d56d20aa9c52dc7e1eb00
vagrant <vagrant@homestead>
2020-03-20 15:50:38 +0000

    wechat

--- resources/views/pages/subscribe.blade.php
+++ resources/views/pages/subscribe.blade.php
@@ -13,7 +13,7 @@
 
 <div class="page">
   <div class="page__hd">
-    <h5 class="page__title">您需要首先关注我们的公众号：长按识别二维码，或者微信搜索 “<span>中小学生阅读推广</span>”</h5>
+    <h5 class="page__title">关注公众号后继续：长按下图并识别二维码，或者微信搜索 “<span>中小学生阅读推广</span>”</h5>
       <p class="page__desc"><img src="https://book.edushu.co/uploads/images/wechat1.png" width="100%"/></p>
   </div>
 
